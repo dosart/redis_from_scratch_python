9ab253d0f6bf4ab7522b535754cfd6d1b3926748
dosart <Dysprosium92@ya.ru>
2023-08-10 20:36:24 +0300

    build: Implements a method write response to deserialize a string into objects

--- redis_from_scratch_python/protocol_handler.py
+++ redis_from_scratch_python/protocol_handler.py
@@ -1,4 +1,5 @@
-from exceptions import ServerDisconnect, CommandError, Error
+from redis_from_scratch_python.exception import ServerDisconnect, CommandError, Error
+from io import BytesIO
 
 
 class ProtocolHandler(object):
@@ -50,4 +51,33 @@ class ProtocolHandler(object):
         return dict(zip(elements[::2], elements[1::2]))
 
     def write_response(self, socket_file, resp):
-        pass
+        def write_response(self, socket_file, data):
+            buf = BytesIO()
+            self._write(buf, data)
+            buf.seek(0)
+            socket_file.write(buf.getvalue())
+            socket_file.flush()
+
+        def _write(self, buf, data):
+            if isinstance(data, str):
+                data = data.encode('utf-8')
+
+            if isinstance(data, bytes):
+                buf.write('$%s\r\n%s\r\n' % (len(data), data))
+            elif isinstance(data, int):
+                buf.write(':%s\r\n' % data)
+            elif isinstance(data, Error):
+                buf.write('-%s\r\n' % error.message)
+            elif isinstance(data, (list, tuple)):
+                buf.write('*%s\r\n' % len(data))
+                for item in data:
+                    self._write(buf, item)
+            elif isinstance(data, dict):
+                buf.write('%%%s\r\n' % len(data))
+                for key in data:
+                    self._write(buf, key)
+                    self._write(buf, data[key])
+            elif data is None:
+                buf.write('$-1\r\n')
+            else:
+                raise CommandError('unrecognized type: %s' % type(data))
